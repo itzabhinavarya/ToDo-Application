8f124128604cde2f985c70950da1a8ce5a48400f
Joseph Cherian <41300035+JCherry101@users.noreply.github.com>
2026-04-16 15:57:29 +0400

    Modify server start condition for app.listen

--- todoServer.js
+++ todoServer.js
@@ -285,8 +285,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`App is listening on http://localhost:${PORT}`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`App is listening on http://localhost:${PORT}`);
+  });
+}
 
 module.exports = app;
